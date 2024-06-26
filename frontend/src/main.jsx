d5bdccd1a8d8fc67e3f25ed0b5145270dac9dff6
yagmuxr <yagmuryalcin26@hotmail.com>
2024-06-26 22:53:20 +0300

    yagmuryalcin-frontend-backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import Layout from "./frontend/src/components/Layouts/Layout";
-import CartProvider from "./frontend/src/context/CartProvider";
+import Layout from "./components/Layouts/Layout";
+import CartProvider from "./context/CartProvider";
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
