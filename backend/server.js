ff65aa7185417581084d4f61f238b7118d271005
yagmur <yagmuryalcin26@hotmail.com>
2024-06-27 14:39:40 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,6 @@ const cors = require("cors");
 
 const mainRoute = require("./routes/index"); // Ensure this file exports a valid Express router
 const userRoute = require("./routes/users"); // Ensure this file exports a valid Express router
-
-import path from 'path';
-import { fileURLToPath } from 'url';
- const __filename = fileURLToPath(import.meta.url);
- const __dirname= path.dirname(__filename);
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -37,7 +32,10 @@ mongoose.connection.on("connected", () => {
 });
 
 // Middleware setup
-app.use(cors({ origin: 'https://esderi.onrender.com' })); // Update with your frontend URL
+app.use(cors({ origin: ['https://esderi.vercel.app/' ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+})); // Update with your frontend URL
 app.use(express.json());
 
 // Routes setup
