ab001c8cd55d7235033ff606ee72c578680755fc
yagmur <yagmuryalcin26@hotmail.com>
2024-06-27 18:33:17 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const logger = require("morgan");
 const mainRoute = require("./routes/index.js");
+const productsRoute = require("./routes/products.js");
 const port = 5000;
 
 dotenv.config();
