7bd9330e74f2c74daf69f05d27c930f453275740
yagmur <yagmuryalcin26@hotmail.com>
2024-06-27 14:17:53 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ const cors = require("cors");
 const mainRoute = require("./routes/index"); // Ensure this file exports a valid Express router
 const userRoute = require("./routes/users"); // Ensure this file exports a valid Express router
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+ const __filename = fileURLToPath(import.meta.url);
+ const __dirname= path.dirname(__filename);
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
