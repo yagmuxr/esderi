cf21c5f2364f1cf681e631ff6785582400e6adc5
yagmur <yagmuryalcin26@hotmail.com>
2024-06-27 17:53:03 +0300

    server

--- backend/server.js
+++ backend/server.js
@@ -1,67 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const app = express();
 const cors = require("cors");
-const path = require("path");
-
-const mainRoute = require("./routes/index");
-const userRoute = require("./routes/users");
+const logger = require("morgan");
+const mainRoute = require("./routes/index.js");
+const port = 5000;
 
 dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
 const connect = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("Connected to MongoDB.");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        throw error;
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDb");
+  } catch (error) {
+    throw error;
+  }
 };
 
-mongoose.connection.on("disconnected", () => {
-    console.log("MongoDB disconnected!");
-});
-
-mongoose.connection.on("connected", () => {
-    console.log("MongoDB connected!");
-});
-
-// Middleware setup
+// middlewares
+app.use(logger("dev"));
+app.use(express.json());
 app.use(cors({
     origin: ['https://esderi.vercel.app'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
-app.use(express.json());
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-// Routes setup
 app.use("/api", mainRoute);
-app.use("/api/users", userRoute);
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+app.listen(port, () => {
+  connect();
+  console.log(`Sunucu ${port} portunda çalışıyor.`);
 });
-
-// Start the server
-app.listen(PORT, async () => {
-    try {
-        await connect();
-        console.log(`Server is running on port ${PORT}`);
-    } catch (error) {
-        console.error("Failed to connect to MongoDB:", error);
-    }
-});
-
-module.exports = app;
